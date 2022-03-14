f18af7550da9f3620edec689803a179c82efa6e4
Joseph Tharpe <solowun6@aol.com>
2022-03-13 22:52:07 -0700

    first commit

---
+++ src/com/codewithjoey/Main.java
@@ -0,0 +1,14 @@
+package com.codewithjoey;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        greetUser("jo", "tee");
+    }
+
+    public static void greetUser(String firstName, String lastName){
+        System.out.println("hey " + firstName + " " + lastName);
+    }
+}
